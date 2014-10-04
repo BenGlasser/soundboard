505d0c6ff3adcf051fe5de1765f4a469d1709d0c
Ben Glasser <ben@benglasser.com>
2014-10-03 22:59:15 -0600

    update git clone

---
+++ app/build/generated/source/buildConfig/release/com/benglasser/devsoundboard/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.benglasser.devsoundboard;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String PACKAGE_NAME = "com.benglasser.javasoundboard";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
